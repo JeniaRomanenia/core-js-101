06a0a0c3a70bab36e74efbf827d3dd9444454f38
JeniaRomanenia <jenia02041985@gmail.com>
2022-07-24 22:32:50 +0300

    fix bags

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -110,7 +110,11 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (1,2)     => 0
  */
 function getAngleBetweenVectors(x1, y1, x2, y2) {
-  return Math.acos((x1 * x2 + y1 * y2) / Math.sqrt(x1 ** 2 + y1 ** 2) * Math.sqrt(x2 ** 2 + y2 ** 2));
+  const scalar = x1 * x2 + y1 * y2;
+  const a = x1 ** 2 + y1 ** 2;
+  const b = x2 ** 2 + y2 ** 2;
+
+  return Math.acos(scalar / (Math.sqrt(a) * Math.sqrt(b)));
 }
 
 /**
